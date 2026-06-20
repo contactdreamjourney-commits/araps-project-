e3a80e5d87d15d37794430895571fc311faa1123
Khurram.se <contact.dreamjourney@gmail.com>
2026-06-20 09:55:32 -0700

    Fix database settings

--- result_system/settings.py
+++ result_system/settings.py
@@ -77,14 +77,13 @@ WSGI_APPLICATION = 'result_system.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
